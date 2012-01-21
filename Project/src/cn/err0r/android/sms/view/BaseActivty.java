cb8207b0b1021ea89ee4603ebe9f40da072e9c48
Tracy Xue <tracyxuening@gmail.com>
2012-01-21 13:01:03 +0800

    add project file and SmsOpertionEx.apk
    Signed-off-by: Tracy Xue <tracyxuening@gmail.com>

---
+++ Project/src/cn/err0r/android/sms/view/BaseActivty.java
@@ -0,0 +1,12 @@
+package cn.err0r.android.sms.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BaseActivty extends Activity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);		
+	}  
+}
